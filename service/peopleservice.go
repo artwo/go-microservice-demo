8467c25ae49561fed0963c725bf05cf551eb046b
artwo <arturo.rivera@metronom.com>
2019-06-01 01:52:40 +0200

    Reworked service for new interface.

--- service/peopleservice.go
+++ service/peopleservice.go
@@ -6,30 +6,43 @@ import (
 	"errors"
 )
 
-func GetPeople() []model.Person {
-	return repo.GetAllPeople()
+type peopleService struct {
+	PeopleRepo repo.PeopleRepository
 }
 
-func GetPerson(personID string) model.Person {
-	return repo.FindPersonByID(personID)
+func NewPeopleService(peopleRepo repo.PeopleRepository) PeopleService {
+	return &peopleService{peopleRepo}
 }
 
-func AddPerson(person model.PersonRequest) error {
+func (s *peopleService) GetAllPeople() ([]model.Person, error) {
+	return s.PeopleRepo.GetAll()
+}
+
+func (s *peopleService) GetPerson(personID string) (model.Person, error) {
+	return s.PeopleRepo.FindByID(personID)
+}
+
+func (s *peopleService) AddPerson(person model.PersonNoID) error {
 	newPerson := model.Person{
 		ID:        "123",
 		Firstname: person.Firstname,
 		Lastname:  person.Lastname,
 		Address:   person.Address,
 	}
-	err := repo.CreatePerson(newPerson)
-	return errors.New("Unable to create person error: '" + err.Error() + "'.")
+	if err := s.PeopleRepo.Add(newPerson); err != nil {
+		return errors.New("Unable to create person error: '" + err.Error() + "'.")
+	}
+	return nil
 }
 
-func RemovePerson(personID string) error {
-	person := repo.FindPersonByID(personID)
+func (s *peopleService) RemovePerson(personID string) error {
+	person, err := s.PeopleRepo.FindByID(personID)
+	if err != nil {
+		return err
+	}
 	if (person == model.Person{}) {
 		return errors.New("Unable to find person with ID '" + personID + "'")
 	}
 
-	return repo.DeletePerson(person)
+	return s.PeopleRepo.Remove(person)
 }
