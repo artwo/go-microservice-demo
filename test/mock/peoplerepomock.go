d724dda685fa962a8d4a2c63f94a6b0c2b157a9a
artwo <arturo.rivera@metronom.com>
2019-06-16 20:11:52 +0200

    Fixed issues in repo mock

--- test/mock/peoplerepomock.go
+++ test/mock/peoplerepomock.go
@@ -11,20 +11,20 @@ type PeopleRepoMock struct {
 
 func (m *PeopleRepoMock) GetAll() ([]model.Person, error) {
 	args := m.Called()
-	return args.Get(0).([]model.Person), args.Error(0)
+	return args.Get(0).([]model.Person), args.Error(1)
 }
 
 func (m *PeopleRepoMock) FindByID(ID string) (model.Person, error) {
-	args := m.Called()
-	return args.Get(0).(model.Person), args.Error(0)
+	args := m.Called(ID)
+	return args.Get(0).(model.Person), args.Error(1)
 }
 
 func (m *PeopleRepoMock) Add(person model.Person) error {
-	args := m.Called()
+	args := m.Called(person)
 	return args.Error(0)
 }
 
 func (m *PeopleRepoMock) Remove(person model.Person) error {
-	args := m.Called()
+	args := m.Called(person)
 	return args.Error(0)
 }
