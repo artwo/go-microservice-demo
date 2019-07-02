1d5c9baa5d16ad6ef13859578c386497dab2c2b3
artwo <arturo.rivera@metronom.com>
2019-07-02 22:19:45 +0200

    Made controller testable. By adding public functions.

--- controller/employeecontroller.go
+++ controller/employeecontroller.go
@@ -27,54 +27,61 @@ func NewController() *Controller {
 	}
 }
 
+func NewTestController(peopleRepo repo.PeopleRepository, peopleService service.PeopleService) *Controller {
+	return &Controller{
+		peopleRepo,
+		peopleService,
+	}
+}
+
 func (c *Controller) Routes() *chi.Mux {
 	router := chi.NewRouter()
-	router.Get("/", c.getAllPeople)
-	router.Get("/{personID}", c.getPerson)
-	router.Post("/", c.postPerson)
-	router.Delete("/{personID}", c.deletePerson)
+	router.Get("/", c.GetAllPeople)
+	router.Get("/{personID}", c.GetPerson)
+	router.Post("/", c.PostPerson)
+	router.Delete("/{personID}", c.DeletePerson)
 	return router
 }
 
-func (c *Controller) getAllPeople(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) GetAllPeople(w http.ResponseWriter, r *http.Request) {
 	people, err := c.PeopleService.GetAllPeople()
 	if err != nil {
 		log.Printf("Unable to get all people: %s\n", err.Error())
-		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 	render.JSON(w, r, people)
 }
 
-func (c *Controller) getPerson(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) GetPerson(w http.ResponseWriter, r *http.Request) {
 	personID := chi.URLParam(r, "personID")
 	person, err := c.PeopleService.GetPerson(personID)
 	if err != nil {
 		log.Printf("Unable to get person: %s\n", err.Error())
-		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 	if (person == model.Person{}) {
 		log.Printf("Unable to find person with ID: %s\n", personID)
-		http.Error(w, "Person not found.", http.StatusNotFound)
+		http.Error(w, "Person not found", http.StatusNotFound)
 		return
 	}
 	render.JSON(w, r, person)
 }
 
-func (c *Controller) postPerson(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) PostPerson(w http.ResponseWriter, r *http.Request) {
 	var personRequest model.PersonNoID
 	err := json.NewDecoder(r.Body).Decode(&personRequest)
 	if err != nil {
 		log.Printf("Unable to parse CreatePerson request body: %s\n", err.Error())
-		http.Error(w, "Wrong body format or element missing in body.", http.StatusBadRequest)
+		http.Error(w, "Wrong body format or element missing in body", http.StatusBadRequest)
 		return
 	}
 
 	err = c.PeopleService.AddPerson(personRequest)
 	if err != nil {
 		log.Printf("Unable to store new person: %s\n", err.Error())
-		http.Error(w, "Something went wrong.", http.StatusInternalServerError)
+		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		return
 	}
 
@@ -84,7 +91,7 @@ func (c *Controller) postPerson(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, response)
 }
 
-func (c *Controller) deletePerson(w http.ResponseWriter, r *http.Request) {
+func (c *Controller) DeletePerson(w http.ResponseWriter, r *http.Request) {
 	personID := chi.URLParam(r, "personID")
 	response := make(map[string]string)
 
@@ -92,11 +99,11 @@ func (c *Controller) deletePerson(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Unable to find person with ID: %s\n", personID)
 		//TODO: render Errors with renderer
-		http.Error(w, "Person not found.", http.StatusNotFound)
+		http.Error(w, "Person not found", http.StatusNotFound)
 		return
 	}
 
 	log.Printf("Deleted person " + personID)
-	response["message"] = "Person deleted successfully."
+	response["message"] = "Person deleted successfully"
 	render.JSON(w, r, response)
 }
