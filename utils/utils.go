a93d86f7e6ec8f5bfa19ff51c882f12dd83e4439
artwo <arturo.rivera@metronom.com>
2019-07-02 22:21:00 +0200

    Added util ToIoReader.

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 )
@@ -11,4 +12,12 @@ func ToString(v interface{}) string {
 		log.Printf("Unable to Marshal interface %T", v)
 	}
 	return string(out)
+}
+
+func ToIoReader(v interface{}) *bytes.Reader {
+	vBytes, err := json.Marshal(v)
+	if err != nil {
+		log.Printf("Unable to Marshal interface %T", v)
+	}
+	return bytes.NewReader(vBytes)
 }
